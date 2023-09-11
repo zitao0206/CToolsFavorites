dc6b7e528840db0f9bb5ff84392f3ef9079bead4
lizitao <634376133@qq.com>
2023-09-11 15:20:00 +0200

    initalize the project

---
+++ CToolsFavorites/CToolsFavoritesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CToolsFavoritesApp.swift
+//  CToolsFavorites
+//
+//  Created by lizitao on 2023/9/11.
+//
+
+import SwiftUI
+
+@main
+struct CToolsFavoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
